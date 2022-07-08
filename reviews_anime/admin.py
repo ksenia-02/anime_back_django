1db32cce146bbbc30a46575ba7f8fe640db45aae
ksenia_art <ksenksenia02@gmail.com>
2022-07-09 01:47:17 +0300

    register models

--- reviews_anime/admin.py
+++ reviews_anime/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Genre)
+admin.site.register(Anime)
+admin.site.register(RaitingStar)
+admin.site.register(Raiting)
+admin.site.register(Reviews)
